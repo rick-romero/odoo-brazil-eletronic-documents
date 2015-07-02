7f64b1dea4814219fd3c001fbacf8f1f7fb76ee9
sadamo <daniel.sadamo@kmee.com.br>
2015-07-02 18:28:55 -0300

    [NEW] edicao do XmlTicket para imprimir NFC-e (faltando adicionar a sessao do QR code)

--- nfce/static/src/js/models.js
+++ nfce/static/src/js/models.js
@@ -37,7 +37,6 @@ openerp.nfce = function (instance) {
             // l10n_br_openerp_pos_models
             this.chNFe = 'chave123456';
             this.nVersao = null;
-            this.tpAmb = this.pos.company.nfe_environment;
             this.cDest = null;
             this.dhEmi = null;
             this.vNF = null;
@@ -45,34 +44,60 @@ openerp.nfce = function (instance) {
             this.digVal = null;
             this.cldToken = null;
             this.cHashQR = null;
-            this.legal_name = this.pos.company.legal_name;
+            this.infCpl = null;
             //
             this.internal_number = null;
             this.serie_nfce = null;
             this.protocolo = null;
             this.vTribTotal = null;
+
             this.qtItens = null;
+            this.tpAmb = this.pos.company.nfe_environment;
+            this.cnpj = this.pos.company.cnpj_cpf;
+            this.inscr_est = this.pos.company.inscr_est;
+            this.inscr_mun = this.pos.company.inscr_mun;
+            this.legal_name = this.pos.company.legal_name;
+            this.company_street = this.pos.company.street;
+            this.company_number = this.pos.company.number;
+            this.company_street2 = this.pos.company.street2;
+            this.company_district = this.pos.company.district;
+            this.company_state = this.pos.company.state_id[1];
+            this.company_city = this.pos.company.l10n_br_city_id[1];
+            this.company_zip = this.pos.company.zip;
 
             return this;
         },
 
-        //get_vICMS: function () {
-        //    var vICMS = 0.0;
-        //    var pos = this.pos;
-        //    this.get('orderLines').each(function (line) {
-        //        var ldetails = line.get_tax_details();
-        //        console.log(JSON.stringify(this));
-        //        console.log(JSON.stringify(this));
-        //        for (var id in ldetails) {
-        //            if (ldetails.hasOwnProperty(id)) {
-        //                if (pos.taxes_by_id[id] == 'icms') {
-        //                    vICMS += ldetails[id]
-        //                }
-        //            }
-        //        }
-        //    });
-        //    return vICMS;
-        //},
+        get_vTribTotal: function () {
+            var vTribTotal = 0.0;
+            var pos = this.pos;
+            this.get('orderLines').each(function (line) {
+                var ldetails = line.get_tax_details();
+                for (var id in ldetails) {
+                    vTribTotal += ldetails[id]
+                }
+            });
+            return vTribTotal;
+        },
+
+        get_mensagemFiscal: function (){
+            var mensagemFiscal = '';
+            var tpAmb = this.tpAmb;
+
+            if (tpAmb == 2){
+                mensagemFiscal = "EMITIDA EM AMBIENTE DE HOMOLOGAÇÃO – SEM VALOR FISCAL";
+            }
+            if (tpAmb == 9){
+                mensagemFiscal = "EMITIDA EM CONTINGÊNCIA"
+            }
+            return mensagemFiscal;
+        },
+
+        get_danfe_dhEmi: function (){
+            var data = new Date();
+            var dhEmi = data.toLocaleDateString()+ ' '+ data.toLocaleTimeString();
+            return dhEmi;
+        },
 
         get_qtItens: function () {
             var qtItens = 0;
@@ -82,69 +107,98 @@ openerp.nfce = function (instance) {
             });
             return qtItens;
         },
-        export_as_JSON: function() {
-            var orderLines, paymentLines;
-            orderLines = [];
-            (this.get('orderLines')).each(_.bind( function(item) {
-                return orderLines.push([0, 0, item.export_as_JSON()]);
-            }, this));
-            paymentLines = [];
-            (this.get('paymentLines')).each(_.bind( function(item) {
-                return paymentLines.push([0, 0, item.export_as_JSON()]);
-            }, this));
-            return {
-                name: this.getName(),
-                amount_paid: this.getPaidTotal(),
-                amount_total: this.getTotalTaxIncluded(),
-                amount_tax: this.getTax(),
-                amount_return: this.getChange(),
-                lines: orderLines,
-                statement_ids: paymentLines,
-                pos_session_id: this.pos.pos_session.id,
-                partner_id: this.get_client() ? this.get_client().id : false,
-                user_id: this.pos.cashier ? this.pos.cashier.id : this.pos.user.id,
-                uid: this.uid,
-                sequence_number: this.sequence_number,
-                vICMS: 6,
-                qtItens: this.get_qtItens(),
-                chNFe: this.chNFe,
-            };
-        }
+
+        get_via: function () {
+            var via = '';
+            if (this.tpAmb == 9){
+                via = {1: "Via do Consumidor", 2: "Via Estabelecimento"};
+            }
+            return via;
+        },
+
+        get_endEletronico: function () {
+            var endEletronico = '';
+            if (this.company_state == "Rio Grande do Sul") {
+                endEletronico = 'www.sefaz.rs.gov.br';
+            }
+            return endEletronico;
+        },
+
+        get_company_address: function() {
+            var company_address = '';
+            if (this.company_street) {
+                company_address += this.company_street;
+            }
+             if (this.company_number) {
+                company_address += ", "+this.company_number;
+            }
+             if (this.company_street2) {
+                company_address += ", "+this.company_street2;
+            }
+             if (this.company_district) {
+                company_address += ", "+ this.company_district;
+            }
+             if (this.company_city) {
+                company_address +=", "+this.company_city;
+            }
+             if (this.company_state) {
+                company_address +=", "+this.company_state;
+            }
+             if (this.company_zip) {
+                 company_address += " - "+this.company_zip;
+             }
+            return company_address
+        },
 
     });
+    var _super_export_for_printing_ = module.Order.prototype.export_for_printing;
+    module.Order.prototype.export_for_printing = function(){
+        res = _super_export_for_printing_.call(this);
+        res.vTribTotal = this.get_vTribTotal();
+        res.company.address = this.get_company_address();
+        res.chNFe = this.chNFe;
+        res.nVersao = this.nVersao;
+        res.cDest = this.cDest;
+        res.dhEmi = this.dhEmi;
+        res.vNF = this.vNF;
+        res.vICMS = this.vICMS;
+        res.digVal = this.digVal;
+        res.cldToken = this.cldToken;
+        res.cHashQR = this.cHashQR;
+        res.infCpl = this.infCpl;
+        res.internal_number = this.internal_number;
+        res.serie_nfce = this.serie_nfce;
+        res.protocolo = this.protocolo;
+        res.qtItens = this.get_qtItens();
+        res.tpAmb = this.tpAmb;
+        res.company.cnpj = this.cnpj;
+        res.company.inscr_est = this.inscr_est;
+        res.mensagemFiscal = this.get_mensagemFiscal();
+        res.via = this.get_via();
+        res.endEletronico = this.get_endEletronico();
+        res.dhEmi = this.get_danfe_dhEmi();
+        res.company.inscr_mun = this.inscr_mun;
+        res.company.legal_name = this.legal_name;
+        console.log(JSON.stringify(res));
+        return res;
+    };
 
-    //var _initialize_ = module.PosModel.prototype.initialize;
-    //module.PosModel.prototype.initialize = function(session, attributes){
-    //    self = this;
-    //    model = {
-    //        model: 'product.template',
-    //        fields: [
-    //            'name',
-    //            'display_name',
-    //            'product_variant_ids',
-    //            'product_variant_count',
-    //            ],
-    //        domain:  function(self){
-    //            return [
-    //                ['sale_ok','=',true],
-    //                ['available_in_pos','=',true],
-    //            ];},
-    //        context: function(self){
-    //            return {
-    //                pricelist: self.pricelist.id,
-    //                display_default_code: false,
-    //            };},
-    //        loaded: function(self, templates){
-    //             self.db.add_templates(templates);
-    //        },
-    //    }
-    //    this.models.push(model);
-    //    return _initialize_.call(this, session, attributes);
-    //};
+
+    //modificação para exibir o recibo ao inves do ticket
+    var _super_receipt_refresh_ = module.ReceiptScreenWidget.prototype.refresh;
+    module.ReceiptScreenWidget.prototype.refresh = function() {
+        var order = this.pos.get('selectedOrder');
+        var receipt = order.export_for_printing();
+        $('.pos-receipt-container', this.$el).html(QWeb.render('XmlReceipt',{
+                        receipt: receipt, widget: self,
+                    }));
+    };
 
     module.PosModel.prototype.models.push({
          model:  'res.company',
-            fields: [ 'currency_id', 'email', 'website', 'company_registry', 'vat', 'name', 'phone', 'partner_id' , 'country_id','legal_name', 'nfe_environment'],
+            fields: [ 'currency_id', 'email', 'website', 'company_registry', 'vat', 'name', 'phone', 'partner_id' , 'country_id',
+                'legal_name', 'nfe_environment', 'cnpj_cpf', 'inscr_est', 'inscr_mun',
+                'street', 'number', 'street2', 'district', 'state_id', 'l10n_br_city_id', 'zip', 'country_id' ],
             ids:    function(self){ return [self.user.company_id[0]] },
             loaded: function(self,companies) {
                 self.company = companies[0];
