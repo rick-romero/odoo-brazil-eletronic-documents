0b9cb2ede4f95b2b570a6de51beb633701d7c521
Danimar Ribeiro <danimaribeiro@gmail.com>
2015-08-09 01:44:40 -0300

    [WIP] Migração para nova api, criado novo campo para criação do fornecedor

--- nfe_import/wizard/l10n_br_account_invoice_import.py
+++ nfe_import/wizard/l10n_br_account_invoice_import.py
@@ -1,9 +1,10 @@
-# coding: utf-8
+# coding=utf-8
 # ###########################################################################
 #
 #    Author: Luis Felipe Mileo
 #            Fernando Marcato Rodrigues
 #            Daniel Sadamo Hirayama
+#            Danimar Ribeiro <danimaribeiro@gmail.com>
 #    Copyright 2015 KMEE - www.kmee.com.br
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -22,35 +23,32 @@
 ##############################################################################
 
 import logging
-from openerp.osv import orm, fields
+from openerp import models, fields, api
 from openerp.tools.translate import _
 from openerp.addons.nfe.sped.nfe.nfe_factory import NfeFactory
 from openerp.exceptions import Warning
 
 import os
 
+
 _logger = logging.getLogger(__name__)
 
 
-class NfeImportAccountInvoiceImport(orm.TransientModel):
+class NfeImportAccountInvoiceImport(models.TransientModel):
     """
         Assistente de importaçao de txt e xml
     """
     _name = 'nfe_import.account_invoice_import'
     _description = 'Import Eletronic Document in TXT and XML format'
 
-    _columns = {
-        'edoc_input': fields.binary(u'Arquivo do documento eletrônico',
-                                   help=u'Somente arquivos no formato TXT e '
-                                        u'XML'),
-        'file_name': fields.char('File Name', size=128),
-        'state': fields.selection([('init', 'init'),
-                                   ('done', 'done')], 'state', readonly=True),
-    }
-
-    _defaults = {
-        'state': 'init',
-    }
+    edoc_input = fields.Binary(u'Arquivo do documento eletrônico',
+                               help=u'Somente arquivos no formato TXT e XML')
+    file_name = fields.Char('File Name', size=128)
+    create_partner = fields.Boolean(
+        u'Criar fornecedor automaticamente?', default=True,
+        help=u'Cria o fornecedor automaticamente caso não esteja cadastrado')
+    state = fields.Selection([('init', 'init'), ('done', 'done')],
+                             string='state', readonly=True, default='init')
 
     def _check_extension(self, filename):
         (__, ftype) = os.path.splitext(filename)
@@ -58,45 +56,53 @@ class NfeImportAccountInvoiceImport(orm.TransientModel):
             raise Warning(_('Please use a file in extensions TXT or XML'))
         return ftype
 
-
     def _get_nfe_factory(self, nfe_version):
         return NfeFactory().get_nfe(nfe_version)
 
-    def import_edoc(self, cr, uid, req_id, context=False):
-        """
-        :param cr:
-        :param uid:
-        :param ids:
-        :param context:
-        :return:
-        """
+    @api.multi
+    def import_edoc(self, req_id, context=False):
         try:
-            context = context or {}
-    
-            if isinstance(req_id, list):
-                req_id = req_id[0]
-    
-            importer = self.browse(cr, uid, req_id, context)
+            self.ensure_one()
+            importer = self[0]
+
             ftype = self._check_extension(importer.file_name)
-    
-            user = self.pool.get('res.users').browse(cr, uid, uid, context=context)
-            edoc_obj = self._get_nfe_factory(user.company_id.nfe_version)
-    
+
+            edoc_obj = self._get_nfe_factory(
+                self.env.user.company_id.nfe_version)
+
             # TODO: Tratar mais de um documento por vez.
-            edoc = edoc_obj.import_edoc(cr, uid, importer.edoc_input,
-                                                ftype, context)[0]
-    
+            eDoc = edoc_obj.import_edoc(
+                self._cr, self._uid, importer.edoc_input, ftype, context)[0]
+
+            inv_values = eDoc['values']
+            if importer.create_partner and inv_values['partner_id'] == False:
+                partner = self.env['res.partner'].create(
+                    inv_values['partner_values'])
+                inv_values['partner_id'] = partner.id
+            elif inv_values['partner_id'] == False:
+                raise Exception(
+                    u'Fornecedor não cadastrado, o xml não será importado\n'\
+                    u'Marque a opção "Criar fornecedor" se deseja importar mesmo assim')
+
+            invoice_id = self.env['account.invoice'].create(inv_values)
+
             model_obj = self.pool.get('ir.model.data')
             action_obj = self.pool.get('ir.actions.act_window')
             action_id = model_obj.get_object_reference(
-                cr, uid, edoc['action'][0], edoc['action'][1])[1]
-            res = action_obj.read(cr, uid, action_id)
-            res['domain'] = res['domain'][:-1] + ",('id', 'in', %s)]" % [edoc['id']]
+                self._cr, self._uid, eDoc['action'][0], eDoc['action'][1])[1]
+            res = action_obj.read(self._cr, self._uid, action_id)
+            res['domain'] = res['domain'][:-1] + \
+                ",('id', 'in', %s)]" % [invoice_id]
             return res
         except Exception as e:
-            _logger.error(unicode(str(e), 'utf-8'), exc_info=True)
-            raise Warning('Erro ao tentar importar o xml\nMensagem de erro:\n{0}'.format(e.message))
-            
+            if isinstance(e.message, unicode):
+                _logger.error(e.message, exc_info=True)
+            else:
+                _logger.error(unicode(e.message, 'utf-8'), exc_info=True)
+            raise Warning(
+                u'Erro ao tentar importar o xml\nMensagem de erro:\n{0}'.format(
+                    e.message))
 
+    @api.multi
     def done(self, cr, uid, ids, context=False):
         return True
